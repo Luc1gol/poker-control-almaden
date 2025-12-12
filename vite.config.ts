39207fd65b5516e6f8f426cf4d4bea28fded7348
Luc1gol <lucianoheringer97@gmail.com>
2025-12-12 03:08:22 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,8 +11,8 @@ export default defineConfig(({ mode }) => {
       },
       plugins: [react()],
       define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+     'process.env.API_KEY': JSON.stringify("teste123"),
+'process.env.GEMINI_API_KEY': JSON.stringify("teste123")
       },
       resolve: {
         alias: {
